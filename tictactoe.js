c3eecb0118b600ff7b9d78e74a37207298e4fdee
raynertjx <rayner742@gmail.com>
2022-01-06 12:22:37 +0800

    Added checkWinner function

--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,17 @@
 var stage = document.querySelector("#stage");
+var playerTurnText = document.querySelector("#playerturntext");
 var gameBoardArr = [
   " ", " ", " ",
   " ", " ", " ",
   " ", " ", " "
 ];
+var CELL_COUNT = 0;
 var SIZE = 100;
 var SPACE = 10;
 var ROWS = 3
 var COLUMNS = 3;
 var playerTurn = true;
+
 
 // player factory function 
 const createPlayer = (name, symbol) => {
@@ -22,33 +25,72 @@ const createPlayer = (name, symbol) => {
 var gameBoard = (function() {
   'use strict';
   
-  function createGameBoard(symbol1, symbol2) {
+  function createGameBoard(player1, player2) {
     for (var row = 0; row < ROWS; row++) {
       for (var column = 0; column < COLUMNS; column++) {
         var cell = document.createElement("div");
         cell.setAttribute("class", "cell");
+        cell.setAttribute("id", CELL_COUNT);
         stage.appendChild(cell);
+        CELL_COUNT++;
        
-
         cell.addEventListener("click", function() {
+          // make sure player cannot play in spot that is already taken
+          if (this.innerText != "") {
+            return;
+          }
+          // alternates between the 2 players
           if (playerTurn) {
-            this.innerHTML = symbol1;
+            this.innerText = player1.symbol;
+            playerTurnText.innerText = `${player1.name}` + "" + "'s Turn";
+            gameBoardArr[this.getAttribute("id")] = player1.symbol
+            console.log(gameBoardArr)
+            displayController.checkWinner(player1.symbol);
           } else {
-            this.innerHTML = symbol2;
+            this.innerText = player2.symbol;
+            playerTurnText.innerText = `${player2.name}` + "" + "'s Turn";
+            gameBoardArr[this.getAttribute("id")] = player2.symbol
+            console.log(gameBoardArr)
+            displayController.checkWinner(player2.symbol);
           }
           playerTurn = !playerTurn;
         });
       }
     }
   }
-
   return {
     createGameBoard: createGameBoard
   };
-
 })();
 
+// displayController module
+var displayController = (function() {
+  'use strict'
 
+  function checkWinner(player) {
+    for (var i = 0; i < 3; i++) {
+      // check the rows for winner
+      if (gameBoardArr[3 * i] === player && gameBoardArr[1 + 3 * i] === player && gameBoardArr[2 + 3 * i] === player) {
+        console.log("yay");
+      }
+      // check the columns for winner
+      if (gameBoardArr[i] === player && gameBoardArr[3 + i] === player && gameBoardArr[6 + i] === player) {
+        console.log("yay");
+      }
+    }
+    // check the first diagonal for winner
+    if (gameBoardArr[0] === player && gameBoardArr[4] === player && gameBoardArr[8] === player) {
+      console.log("yay");
+    }
+    // check the second diagonal for winner
+    if (gameBoardArr[2] === player && gameBoardArr[4] === player && gameBoardArr[6] === player) {
+      console.log("yay");
+    }
+  }
+  return {
+    checkWinner: checkWinner
+  };
+})();
 
 // game object, controls flow of game
 var game = function() {
@@ -58,7 +100,7 @@ var game = function() {
   var player2 = createPlayer(2, "o");
 
   // Create gameboard
-  gameBoard.createGameBoard(player1.symbol, player2.symbol);
+  gameBoard.createGameBoard(player1, player2);
 
 }
 
